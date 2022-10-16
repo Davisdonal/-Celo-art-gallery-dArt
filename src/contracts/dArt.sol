82548ab0cc5a6917c4a04a664b144f2230815d2e
Davisdonal <115899294+Davisdonal@users.noreply.github.com>
2022-10-16 19:36:33 +0100

    Update dArt.sol

--- src/contracts/dArt.sol
+++ src/contracts/dArt.sol
@@ -37,8 +37,6 @@ contract TechArt {
         string image;
         string description;
         uint tip;
-        uint noOfAvailable;
-        uint sold;
     }
 
     mapping(uint => Art) private arts;
@@ -53,35 +51,21 @@ contract TechArt {
         _;
     }
 
-    /**
-     * @dev allow users to add a art to sell
-     * @notice  values entered needs to be valid
-     * @param _noOfAvailable the number of arts available for sale
-     */
     function addArt(
         string calldata _image,
         string calldata _description,
-        uint _tip,
-        uint _noOfAvailable
-    ) public checkIfValidInput(_tip) checkIfValidInput(_noOfAvailable) {
+        uint _tip
+    ) public checkIfValidInput(_tip)  {
         require(bytes(_image).length > 0, "Empty image");
         require(bytes(_description).length > 0, "Empty description");
-        uint _sold = 0;
         arts[artsLength] = Art(
             payable(msg.sender),
             _image,
             _description,
-            _tip,
-            _noOfAvailable,
-            _sold
+            _tip
         );
-        artsLength++;
     }
 
-    /**
-     * @dev allow arts' owners to unlist their arts. Cleanup of gagdet's data occurs
-     * @notice This will remove the art from the platform
-     */
     function unlistArt(uint _index) public checkIfArtOwner(_index) {
         uint newArtsLength = artsLength - 1;
         arts[_index] = arts[newArtsLength];
@@ -89,43 +73,7 @@ contract TechArt {
         artsLength = newArtsLength;
     }
 
-    /**
-     * @dev allow arts' owners to increase their inventory of a art(noOfAvailable)
-     * @param _amount the number to add with the current inventory
-     *
-     */
-    function addInventory(uint _index, uint _amount)
-        public
-        checkIfArtOwner(_index)
-        checkIfValidInput(_amount)
-    {
-        Art storage currentArt = arts[_index];
-        uint newNoOfAvailable = currentArt.noOfAvailable + _amount;
-        currentArt.noOfAvailable = newNoOfAvailable;
-    }
-
-        /**
-     * @dev allow arts' owners to reduce their inventory of a art(noOfAvailable)
-     * @notice Amount to deduct from the current inventory needs to be less or equal to the current inventory
-     * @param _amount the number to deduct from the current inventory
-     */
-    function reduceInventory(uint _index, uint _amount)
-        external
-        checkIfArtOwner(_index)
-    {
-        Art storage currentArt = arts[_index];
-        require(
-            _amount <= currentArt.noOfAvailable,
-            "amount can only be less or equal to the number of arts available"
-        );
-        uint newNoOfAvailable = currentArt.noOfAvailable - _amount;
-        currentArt.noOfAvailable = newNoOfAvailable;
-    }
-
-    /**
-     * @dev allow arts' owners to change the tip of their arts
-     * @notice newTip needs to be greater than zero
-     */
+    
     function modifyTip(uint _index, uint _newTip)
         external
         checkIfArtOwner(_index)
@@ -142,34 +90,21 @@ contract TechArt {
             address payable,
             string memory,
             string memory,
-            uint,
-            uint,
             uint
+            
         )
     {
         return (
             arts[_index].owner,
             arts[_index].image,
             arts[_index].description,
-            arts[_index].tip,
-            arts[_index].noOfAvailable,
-            arts[_index].sold
+            arts[_index].tip
         );
     }
 
-    /**
-     * @dev allow users to buy a art from the platform
-     * @notice Reverts if art is out of inventory(out of stock)
-     */
     function buyArt(uint _index, uint _quantity) public payable checkIfValidInput(_quantity){
         Art storage currentArt = arts[_index];
-        require(currentArt.noOfAvailable > _quantity, "Stocks unavailable");
-        require(
-            currentArt.owner != msg.sender,
-            "You can't buy your own arts"
-        );
-        currentArt.sold+= _quantity;
-        currentArt.noOfAvailable-= _quantity;
+        
         require(
             IERC20Token(cUsdTokenAddress).transferFrom(
                 msg.sender,
@@ -184,4 +119,5 @@ contract TechArt {
     function getArtsLength() public view returns (uint) {
         return (artsLength);
     }
+
 }
